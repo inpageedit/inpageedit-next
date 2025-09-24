65412c00e2d6945d71c7285a2aab356d083533da
dragon-fish <dragon-fish@qq.com>
2025-09-24 19:13:21 +0800

    doc: fix base url

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: './',
   title: 'InPageEdit NEXT',
   description: '🚀 Modular, Extensible Supercharged Plugin for MediaWiki',
   themeConfig: {
