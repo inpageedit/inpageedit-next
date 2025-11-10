827f3efe93496dea958e235dc0e799a2544d192f
dragon-fish <dragon-fish@qq.com>
2025-11-11 02:24:09 +0800

    build(core): enhance types declaration
    - Implemented a custom plugin to fix issues with the generated `index.d.ts` file.

--- packages/core/rolldown.config.ts
+++ packages/core/rolldown.config.ts
@@ -1,14 +1,65 @@
 // rolldown.config.ts
+import { readFile, stat, writeFile } from 'node:fs/promises'
+import { resolve } from 'node:path'
 import { defineConfig } from 'rolldown'
 import { dts } from 'rolldown-plugin-dts'
-import { resolve } from 'node:path'
+import AutoImport from 'unplugin-auto-import/rolldown'
 
 export default defineConfig({
   input: './src/index.ts',
-  plugins: [dts({ sourcemap: false, tsconfig: 'tsconfig.app.json', vue: true, emitDtsOnly: true })],
+  tsconfig: 'tsconfig.app.json',
+  plugins: [
+    AutoImport({
+      dirs: [
+        './src/components/**',
+        './src/constants',
+        './src/utils',
+        './src/polyfills',
+        './src/decorators',
+      ],
+      imports: [
+        {
+          from: '@/plugins/BasePlugin',
+          imports: [['default', 'BasePlugin']],
+        },
+      ],
+      dts: './src/auto-imports.d.ts',
+      dtsMode: 'overwrite',
+    }),
+    dts({ sourcemap: false, tsconfig: 'tsconfig.app.json', vue: true, emitDtsOnly: true }),
+    /**
+     * FIXME:
+     * This is a very dirty hack to fix the index.d.ts file.
+     * @see https://github.com/sxzz/rolldown-plugin-dts/issues/134
+     *
+     * After the index.d.ts file is generated, we need to fix the index.d.ts file,
+     * replace all `declare module '[@.]/.+?'` with `declare module '.'`
+     */
+    {
+      name: 'shamfully-fix-dts-index',
+      async writeBundle(options, bundle) {
+        console.info('shamfully-fix-dts-index', options, bundle)
+        const outDir = (options as any)?.dir || 'dist'
+        const indexDtsPath = resolve(import.meta.dirname, outDir, 'index.d.ts')
+        if (!(await stat(indexDtsPath)).isFile()) return
+        const content = await readFile(indexDtsPath, 'utf-8')
+        const replaced = content.replace(
+          /declare\s+module\s+(['"][@\.]\/.+?['"])\s*\{/g,
+          // prettier-ignore
+          [
+            '//              ↓ $1',
+            "declare module '.' {"
+          ].join('\n')
+        )
+        if (replaced !== content) {
+          await writeFile(indexDtsPath, replaced, 'utf-8')
+        }
+      },
+    },
+  ],
   resolve: {
     alias: {
-      '@': resolve(import.meta.dirname, 'src'),
+      '@': './src',
     },
   },
   // 忽略 .scss/.css/.vue 文件，因为我们只需要暴露 JS API 即可，组件和样式不重要
